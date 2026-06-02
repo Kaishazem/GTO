e195e58560dbb2a145edccd4696176c1b54b7713
Kaishazem <kaishazem13579@gmail.com>
2026-06-02 19:35:13 +0300

    Remove .tsx extension from toaster import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Switch, Route, Router as WouterRouter, Redirect } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster.tsx";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
 import { TaskProvider } from "@/contexts/TaskContext";
